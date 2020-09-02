3cda55ecab22012a908543fbeb9d60a8d6adc3ec
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-09-02 19:48:17 +0530

    Seperate sections
    Now there are  separate sections for HTML, CSS and JS.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 function update() {
-	var text=document.getElementById('editor').value;
-	document.getElementById('viewer').srcdoc=text;
+	console.log("123");
+	let htmlCode=document.getElementById("htmlCode").value;
+	let cssCode=document.getElementById("cssCode").value;
+	let javascriptCode=document.getElementById("javascriptCode").value;
+	let text=htmlCode+"<style>"+cssCode+"</style>"+"<scri"+"pt>"+javascriptCode+"</scri"+"pt>";
+	let iframe=document.getElementById('viewer').contentWindow.document;
+	iframe.open();
+	iframe.write(text);
+	iframe.close();
+	console.log("12345");
 }
